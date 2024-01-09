c656ee869e8f27dfa84df31d2e900e022df09742
Promise <Wilfredo@wilfredolaboy.com>
2024-01-08 23:21:05 -0500

    Switch Statement

--- psychic-train/Assets/_Scripts/BattleScripts/EnemyBattleHUD.cs
+++ psychic-train/Assets/_Scripts/BattleScripts/EnemyBattleHUD.cs
@@ -6,32 +6,32 @@ using UnityEngine.UI;
 public class EnemyBattleHUD : MonoBehaviour
 {
     
-    public TextMeshProUGUI nameText;
-    public TextMeshProUGUI levelText;
-    public Slider hpSlider;
-    //BattleManager battleManager = BattleManager.GetInstance();
-    
-
-    private void Start()
-    {
-
-        SetHud();
-    }
-    
-    
-    public void SetHud()
-    {
-        // nameText.text = battleManager.enemyToLoad[0].unitName;
-        // levelText.text = "Lvl " +battleManager.enemyToLoad[0].unitLevel;
-        // hpSlider.maxValue = battleManager.enemyToLoad[0].maxHP;
-        // hpSlider.value = battleManager.enemyToLoad[0].currentHP;
-    }
-    
-    
-    public void SetHP(int hp)
-    {
-        hpSlider.value = hp;
-    }
-    
+    // public TextMeshProUGUI nameText;
+    // public TextMeshProUGUI levelText;
+    // public Slider hpSlider;
+    // //BattleManager battleManager = BattleManager.GetInstance();
+    //
+    //
+    // private void Start()
+    // {
+    //
+    //     SetHud();
+    // }
+    //
+    //
+    // public void SetHud()
+    // {
+    //     // nameText.text = battleManager.enemyToLoad[0].unitName;
+    //     // levelText.text = "Lvl " +battleManager.enemyToLoad[0].unitLevel;
+    //     // hpSlider.maxValue = battleManager.enemyToLoad[0].maxHP;
+    //     // hpSlider.value = battleManager.enemyToLoad[0].currentHP;
+    // }
+    //
+    //
+    // public void SetHP(int hp)
+    // {
+    //     hpSlider.value = hp;
+    // }
+    //
     
 }
--- psychic-train/Assets/_Scripts/BattleScripts/Unit.cs
+++ psychic-train/Assets/_Scripts/BattleScripts/Unit.cs
@@ -1,27 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-   public UnitStats unit;
-   
-   public string unitName;
-   public int unitLevel;
-
-   public int damage;
-
-   public int maxHP;
-   public int currentHP;
+    public UnitStats stats;
+    
+    // public string unitName ;
+    // public int unitLevel;
+    // public int damage;
+    // public int maxHP;
+    // public int currentHP;
 
 
-   public void Start()
-   {
-      unitName = unit.unitName;
-      unitLevel = unit.unitLevel;
-      damage = unit.damage;
-      maxHP = unit.maxHP;
-      currentHP = unit.currentHP;
-   }
+    private void Awake()
+    {
+        // unitName = stats.unitName;
+        // damage = stats.damage;
+        // unitLevel = stats.unitLevel;
+        // maxHP = stats.maxHP;
+        // currentHP = stats.currentHP;
+    }
+
+    public bool TakeDamage(int dmg)
+    {
+        stats.currentHP = stats.currentHP - dmg;
+        
+
+        if (stats.currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amount)
+    {
+        stats.currentHP += amount;
+        if (stats.currentHP > stats.maxHP)
+        {
+            stats.currentHP = stats.maxHP;
+        }
+    }
+    
+
 }
